841c4b2bfb8dd8afb52f05dfde1d7fc73f1e9953
patelaftab <10patelaftab@gmail.com>
2023-03-14 17:40:16 +0530

    [Aftab]: cheking Whwther a Given Number Is Odd Or Even

---
+++ LogicBuilding/OddEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class OddEven
+    {
+        public static void EvenOddNumbers()
+        {
+            Console.WriteLine("Enter a Number To check Odd or Even");
+            int number=Convert.ToInt32(Console.ReadLine());
+            if(number%2==0)
+            {
+                Console.WriteLine("Given Number is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Given Number is Odd Number");
+            }
+        }
+    }
+}
---
+++ LogicBuilding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programing");
+            bool flag = false;
+            while (!flag)
+            {
+                Console.WriteLine("\nChoose a Option To get Output Of desired Program\n"+"" +
+                    "Choose 1: Check Odd Even Number Program\n");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        OddEven.EvenOddNumbers();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid Options");
+                        break;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
