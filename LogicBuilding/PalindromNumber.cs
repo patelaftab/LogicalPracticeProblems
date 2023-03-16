74611d480bc31a3dbbde0f9e19d005b1786aa1a4
patelaftab <10patelaftab@gmail.com>
2023-03-16 19:06:48 +0530

    [Aftab]: Checking a Number Is Palindrome or Not

---
+++ LogicBuilding/PalindromNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class PalindromNumber
+    {
+        public static void PalindromeNumber()
+        {
+            Console.WriteLine("Enter Number To Check Palindrom ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int temp = number;
+            int reminder, reverse = 0;
+            while (number > 0)
+            {
+                reminder = number % 10;
+                reverse = reverse * 10 + reminder;
+                number = number / 10;
+            }
+            Console.WriteLine("Reversed Number is : {0}", reverse);
+            if (reverse == temp)
+                Console.WriteLine("Given Number is Palindrom Number");
+            else
+                Console.WriteLine("Given Nnumber Is Not a Plaindrom Number");
+        }
+    }
+}
--- LogicBuilding/Program.cs
+++ LogicBuilding/Program.cs
@@ -22,10 +22,11 @@ namespace LogicBuilding
                     "Choose 5: Check Swap Numbers Program\n" +
                     "Choose 6: Check a Number Which IS Devisible By Two\n" +
                     "Choose 7: Check Multiple Of 3 And 5\n" +
-                    "Choose 8: Check Printing Of Multiple Of 17\n"+
-                    "Choose 9: Check Sum Of Digits Of Numbers\n"+
-                    "Choose 10: Check REverse Number Program\n"+
-                    "Choose 11: To Stop running Program\n");
+                    "Choose 8: Check Printing Of Multiple Of 17\n" +
+                    "Choose 9: Check Sum Of Digits Of Numbers\n" +
+                    "Choose 10: Check REverse Number Program\n" +
+                    "Choose 11: Check Number is Palindrom Or Not\n" +
+                    "Choose 12: To Stop running Program\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -60,6 +61,9 @@ namespace LogicBuilding
                         ReveseNumbers.ReversingNumber();
                         break;
                     case 11:
+                        PalindromNumber.PalindromeNumber();
+                        break;
+                    case 12:
                         flag = true;
                         break;
                 }
