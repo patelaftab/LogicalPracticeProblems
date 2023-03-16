2d340d95bfaf2bea98c85fb7680c66776757fd08
patelaftab <10patelaftab@gmail.com>
2023-03-16 13:44:07 +0530

    [Aftab]: Checkinf Multiple Of 3 And 5 And Checking Sum Of Numbers

---
+++ LogicBuilding/CheckingMultiple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class CheckingMultiple
+    {
+        public static void MultipleOfThreeAndfive()
+        {
+            Console.WriteLine("Enter A upper Range Of Numbers To Check Multiple Of 3 And 5");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i, a, b, sum = 0;
+            for (i = 1; i <= number; i++)
+            {
+                a=i%3; b=i%3;
+                if (a == 0 || b == 0)
+                {
+                    Console.Write("{0}\t", i);
+                    sum =sum+ i;
+                }
+               
+            }
+            Console.WriteLine("Sum Of Numbers {0}:", sum);
+        }
+    }
+}
--- LogicBuilding/Program.cs
+++ LogicBuilding/Program.cs
@@ -16,12 +16,13 @@ namespace LogicBuilding
             {
                 Console.WriteLine("\nChoose a Option To get Output Of desired Program\n" + "" +
                     "Choose 1: Check Odd Even Number Program\n" + "" +
-                    "Choose 2: Check Odd Numbers In Given Range\n"+
-                    "Choose 3: Check Giiven Number IS positive Or Not\n"+
+                    "Choose 2: Check Odd Numbers In Given Range\n" +
+                    "Choose 3: Check Giiven Number IS positive Or Not\n" +
                     "Choose 4: Check Largest Among Two Numbers\n" +
-                    "Choose 5: Check Swap Numbers Program\n"+
-                    "Choose 6: Check a Number Which IS Devisible By Two\n"+
-                    "Choose 7: To Stop running Program\n");
+                    "Choose 5: Check Swap Numbers Program\n" +
+                    "Choose 6: Check a Number Which IS Devisible By Two\n" +
+                    "Choose 7: Check Multiple Of 3 And 5\n" +
+                    "Choose 8: To Stop running Program\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -44,6 +45,9 @@ namespace LogicBuilding
                         NumberDivisibleByTwo.NumberCheck();
                         break;
                     case 7:
+                        CheckingMultiple.MultipleOfThreeAndfive();
+                        break;
+                    case 8:
                         flag = true;
                         break;
                 }
